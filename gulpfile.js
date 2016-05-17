678f69f5780152c3ae5875ac6cebcdc6a790aa94
Michael <lssslhs@gmail.com>
2016-05-16 18:49:51 -0700

    update eslint rule and add gulp task to vet

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,15 @@
 'use strict';
 
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+
+gulp.task('vet', function(){
+	return gulp
+		.src([
+			//only vet client code(es6 not well support by all plugins)
+			'./src/client/**/*.js' 
+		])
+		.pipe(eslint())
+		.pipe(eslint.format());
+});
+
